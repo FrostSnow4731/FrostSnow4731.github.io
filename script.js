2005c75d7c0e39297f81e70214d3d4e084a9e328
FrostSnow4731 <ajsj3211@naver.com>
2025-02-22 04:11:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ document.querySelectorAll('.academy').forEach(academy => {
         students.forEach(student => {
           const listItem = document.createElement('li');
           listItem.innerHTML = `
-            <img src="${student.imageUrl}" alt="${student.name}" style="width: 60px; height: 60px; margin-right: 10px; border-radius: 0%;" />
+            <img src="${student.imageUrl}" alt="${student.name}" style="width: 236px; height: 193px; margin-right: 10px; border-radius: 0%;" />
             ${student.name}
           `;
           studentList.appendChild(listItem);
